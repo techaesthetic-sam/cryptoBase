e1ffc80c7ede0e255234231458121881f0e00a8b
techaesthetic-sam <85981274+techaesthetic-sam@users.noreply.github.com>
2024-10-13 18:51:43 +0530

    navbar

--- crytoBase/src/App.jsx
+++ crytoBase/src/App.jsx
@@ -1,14 +1,10 @@
+import { ThemeProvider } from "./context/ThemeContext";
+import Navbar from "./components/Navbar";
 function App() {
   return (
-    <>
-      <h3 className="text-slate-900 dark:text-white mt-5 text-base font-medium tracking-tight">
-        Writes Upside-Down
-      </h3>
-      <p className="text-slate-500 dark:text-slate-400 mt-2 text-lg">
-        The Zero Gravity Pen can be used to write in any orientation, including
-        upside-down. It even works in outer space.
-      </p>
-    </>
+    <ThemeProvider>
+      <Navbar />
+    </ThemeProvider>
   );
 }
 
